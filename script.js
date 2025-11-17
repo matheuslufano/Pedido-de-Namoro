c5bb6806d68ad605a997ed7385da77a4bb3eca6d
Matheus Lufano <matheussantosmota1910@gmail.com>
2025-11-17 02:52:44 -0300

    som dos butões

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-const somClick = new Audio("sons/vitoria.mp3");
 const somFuja = new Audio("sons/erro.mp3");
+const somClick = new Audio("sons/vitoria.mp3");
 
 function fuja() {
-  somFuja.currentTime = 0;
-  somFuja.play();
-  
   var botaoNao = document.getElementById("nao");
 
   var larguraTela = window.innerWidth - botaoNao.offsetWidth;
@@ -13,8 +10,15 @@ function fuja() {
   var novaPosicaoX = Math.random() * larguraTela;
   var novaPosicaoY = Math.random() * alturaTela;
 
+  somFuja.currentTime = 0;
+  somFuja.play();
+
   botaoNao.style.left = novaPosicaoX + "px";
   botaoNao.style.top = novaPosicaoY + "px";
+
+  const botao = document.getElementById("nao") ;
+  botao.style.top = (Math.random() * 300) + "px";
+  botao.style.left = (Math.random() * 300) + "px";
 }
 
 function abrirPopup() {
@@ -24,17 +28,6 @@ document.getElementById("popup").classList.add("show");
 function fecharPopup() {
 document.getElementById("popup").classList.remove("show");
 }
-
-
-function fuja() {
-somFuja.currentTime = 0;
-somFuja.play();
-
-const botao = document.getElementById("nao");
-botao.style.top = (Math.random() * 300) + "px";
-botao.style.left = (Math.random() * 300) + "px";
-}
-
 
 function parabens() {
 somClick.currentTime = 0;
